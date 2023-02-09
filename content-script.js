e022b920e5aee0199dbb7032a733047cb4ff15ea
Cactri R.L <109222990+CactriRL@users.noreply.github.com>
2023-02-09 09:07:03 -0400

    Add files via upload

---
+++ content-script.js
@@ -0,0 +1,246 @@
+		
+Element	=	document.getElementsByClassName("btn btn-link")[1]
+Element2	=	document.getElementsByClassName("btn btn-link")[2]
+Element3	=	document.getElementsByClassName("btn btn-link")[3]
+Element4	=	document.getElementsByClassName("btn btn-link")[4]
+Element5	=	document.getElementsByClassName("btn btn-link")[5]
+Element6	=	document.getElementsByClassName("btn btn-link")[6]
+Element7	=	document.getElementsByClassName("btn btn-link")[7]
+Element8	=	document.getElementsByClassName("btn btn-link")[8]
+Element9	=	document.getElementsByClassName("btn btn-link")[9]
+Element10	=	document.getElementsByClassName("btn btn-link")[10]
+Element11	=	document.getElementsByClassName("btn btn-link")[11]
+Element12	=	document.getElementsByClassName("btn btn-link")[12]
+Element13	=	document.getElementsByClassName("btn btn-link")[13]
+Element14	=	document.getElementsByClassName("btn btn-link")[14]
+Element15	=	document.getElementsByClassName("btn btn-link")[15]
+Element16	=	document.getElementsByClassName("btn btn-link")[16]
+Element17	=	document.getElementsByClassName("btn btn-link")[17]
+Element18	=	document.getElementsByClassName("btn btn-link")[18]
+Element19	=	document.getElementsByClassName("btn btn-link")[19]
+Element20	=	document.getElementsByClassName("btn btn-link")[20]
+Element21	=	document.getElementsByClassName("btn btn-link")[21]
+Element22	=	document.getElementsByClassName("btn btn-link")[22]
+Element23	=	document.getElementsByClassName("btn btn-link")[23]
+Element24	=	document.getElementsByClassName("btn btn-link")[24]
+Element25	=	document.getElementsByClassName("btn btn-link")[25]
+Element26	=	document.getElementsByClassName("btn btn-link")[26]
+Element27	=	document.getElementsByClassName("btn btn-link")[27]
+Element28	=	document.getElementsByClassName("btn btn-link")[28]
+Element29	=	document.getElementsByClassName("btn btn-link")[29]
+Element30	=	document.getElementsByClassName("btn btn-link")[30]
+Element31	=	document.getElementsByClassName("btn btn-link")[31]
+Element32	=	document.getElementsByClassName("btn btn-link")[32]
+Element33	=	document.getElementsByClassName("btn btn-link")[33]
+Element34	=	document.getElementsByClassName("btn btn-link")[34]
+Element35	=	document.getElementsByClassName("btn btn-link")[35]
+Element36	=	document.getElementsByClassName("btn btn-link")[36]
+Element37	=	document.getElementsByClassName("btn btn-link")[37]
+Element38	=	document.getElementsByClassName("btn btn-link")[38]
+Element39	=	document.getElementsByClassName("btn btn-link")[39]
+Element40	=	document.getElementsByClassName("btn btn-link")[40]
+Element41	=	document.getElementsByClassName("btn btn-link")[41]
+Element42	=	document.getElementsByClassName("btn btn-link")[42]
+Element43	=	document.getElementsByClassName("btn btn-link")[43]
+Element44	=	document.getElementsByClassName("btn btn-link")[44]
+Element45	=	document.getElementsByClassName("btn btn-link")[45]
+Element46	=	document.getElementsByClassName("btn btn-link")[46]
+Element47	=	document.getElementsByClassName("btn btn-link")[47]
+Element48	=	document.getElementsByClassName("btn btn-link")[48]
+Element49	=	document.getElementsByClassName("btn btn-link")[49]
+Element50	=	document.getElementsByClassName("btn btn-link")[50]
+Element51	=	document.getElementsByClassName("btn btn-link")[51]
+Element52	=	document.getElementsByClassName("btn btn-link")[52]
+Element53	=	document.getElementsByClassName("btn btn-link")[53]
+Element54	=	document.getElementsByClassName("btn btn-link")[54]
+Element55	=	document.getElementsByClassName("btn btn-link")[55]
+Element56	=	document.getElementsByClassName("btn btn-link")[56]
+Element57	=	document.getElementsByClassName("btn btn-link")[57]
+Element58	=	document.getElementsByClassName("btn btn-link")[58]
+Element59	=	document.getElementsByClassName("btn btn-link")[59]
+Element60	=	document.getElementsByClassName("btn btn-link")[60]
+Element61	=	document.getElementsByClassName("btn btn-link")[61]
+Element62	=	document.getElementsByClassName("btn btn-link")[62]
+Element63	=	document.getElementsByClassName("btn btn-link")[63]
+Element64	=	document.getElementsByClassName("btn btn-link")[64]
+Element65	=	document.getElementsByClassName("btn btn-link")[65]
+Element66	=	document.getElementsByClassName("btn btn-link")[66]
+Element67	=	document.getElementsByClassName("btn btn-link")[67]
+Element68	=	document.getElementsByClassName("btn btn-link")[68]
+Element69	=	document.getElementsByClassName("btn btn-link")[69]
+Element70	=	document.getElementsByClassName("btn btn-link")[70]
+Element71	=	document.getElementsByClassName("btn btn-link")[71]
+Element72	=	document.getElementsByClassName("btn btn-link")[72]
+Element73	=	document.getElementsByClassName("btn btn-link")[73]
+Element74	=	document.getElementsByClassName("btn btn-link")[74]
+Element75	=	document.getElementsByClassName("btn btn-link")[75]
+Element76	=	document.getElementsByClassName("btn btn-link")[76]
+Element77	=	document.getElementsByClassName("btn btn-link")[77]
+Element78	=	document.getElementsByClassName("btn btn-link")[78]
+Element79	=	document.getElementsByClassName("btn btn-link")[79]
+Element80	=	document.getElementsByClassName("btn btn-link")[80]
+Element81	=	document.getElementsByClassName("btn btn-link")[81]
+Element82	=	document.getElementsByClassName("btn btn-link")[82]
+Element83	=	document.getElementsByClassName("btn btn-link")[83]
+Element84	=	document.getElementsByClassName("btn btn-link")[84]
+Element85	=	document.getElementsByClassName("btn btn-link")[85]
+Element86	=	document.getElementsByClassName("btn btn-link")[86]
+Element87	=	document.getElementsByClassName("btn btn-link")[87]
+Element88	=	document.getElementsByClassName("btn btn-link")[88]
+Element89	=	document.getElementsByClassName("btn btn-link")[89]
+Element90	=	document.getElementsByClassName("btn btn-link")[90]
+Element91	=	document.getElementsByClassName("btn btn-link")[91]
+Element92	=	document.getElementsByClassName("btn btn-link")[92]
+Element93	=	document.getElementsByClassName("btn btn-link")[93]
+Element94	=	document.getElementsByClassName("btn btn-link")[94]
+Element95	=	document.getElementsByClassName("btn btn-link")[95]
+Element96	=	document.getElementsByClassName("btn btn-link")[96]
+Element97	=	document.getElementsByClassName("btn btn-link")[97]
+Element98	=	document.getElementsByClassName("btn btn-link")[98]
+Element99	=	document.getElementsByClassName("btn btn-link")[99]
+Element100	=	document.getElementsByClassName("btn btn-link")[100]
+Element101	=	document.getElementsByClassName("btn btn-link")[101]
+Element102	=	document.getElementsByClassName("btn btn-link")[102]
+Element103	=	document.getElementsByClassName("btn btn-link")[103]
+Element104	=	document.getElementsByClassName("btn btn-link")[104]
+Element105	=	document.getElementsByClassName("btn btn-link")[105]
+Element106	=	document.getElementsByClassName("btn btn-link")[106]
+Element107	=	document.getElementsByClassName("btn btn-link")[107]
+Element108	=	document.getElementsByClassName("btn btn-link")[108]
+Element109	=	document.getElementsByClassName("btn btn-link")[109]
+Element110	=	document.getElementsByClassName("btn btn-link")[110]
+Element111	=	document.getElementsByClassName("btn btn-link")[111]
+Element112	=	document.getElementsByClassName("btn btn-link")[112]
+Element113	=	document.getElementsByClassName("btn btn-link")[113]
+Element114	=	document.getElementsByClassName("btn btn-link")[114]
+Element115	=	document.getElementsByClassName("btn btn-link")[115]
+Element116	=	document.getElementsByClassName("btn btn-link")[116]
+Element117	=	document.getElementsByClassName("btn btn-link")[117]
+Element118	=	document.getElementsByClassName("btn btn-link")[118]
+Element119	=	document.getElementsByClassName("btn btn-link")[119]
+Element120	=	document.getElementsByClassName("btn btn-link")[120]
+Element121	=	document.getElementsByClassName("btn btn-link")[121]
+
+
+
+Element.remove()
+Element2.remove()
+Element3.remove()
+Element4.remove()
+Element5.remove()
+Element6.remove()
+Element7.remove()
+Element8.remove()
+Element9.remove()
+Element10.remove()
+Element11.remove()
+Element12.remove()
+Element13.remove()
+Element14.remove()
+Element15.remove()
+Element16.remove()
+Element17.remove()
+Element18.remove()
+Element19.remove()
+Element20.remove()
+Element21.remove()
+Element22.remove()
+Element23.remove()
+Element24.remove()
+Element25.remove()
+Element26.remove()
+Element27.remove()
+Element28.remove()
+Element29.remove()
+Element30.remove()
+Element31.remove()
+Element32.remove()
+Element33.remove()
+Element34.remove()
+Element35.remove()
+Element36.remove()
+Element37.remove()
+Element38.remove()
+Element39.remove()
+Element40.remove()
+Element41.remove()
+Element42.remove()
+Element43.remove()
+Element44.remove()
+Element45.remove()
+Element46.remove()
+Element47.remove()
+Element48.remove()
+Element49.remove()
+Element50.remove()
+Element51.remove()
+Element52.remove()
+Element53.remove()
+Element54.remove()
+Element55.remove()
+Element56.remove()
+Element57.remove()
+Element58.remove()
+Element59.remove()
+Element60.remove()
+Element61.remove()
+Element62.remove()
+Element63.remove()
+Element64.remove()
+Element65.remove()
+Element66.remove()
+Element67.remove()
+Element68.remove()
+Element69.remove()
+Element70.remove()
+Element71.remove()
+Element72.remove()
+Element73.remove()
+Element74.remove()
+Element75.remove()
+Element76.remove()
+Element77.remove()
+Element78.remove()
+Element79.remove()
+Element80.remove()
+Element81.remove()
+Element82.remove()
+Element83.remove()
+Element84.remove()
+Element85.remove()
+Element86.remove()
+Element87.remove()
+Element88.remove()
+Element89.remove()
+Element90.remove()
+Element91.remove()
+Element92.remove()
+Element93.remove()
+Element94.remove()
+Element95.remove()
+Element96.remove()
+Element97.remove()
+Element98.remove()
+Element99.remove()
+Element100.remove()
+Element101.remove()
+Element102.remove()
+Element103.remove()
+Element104.remove()
+Element105.remove()
+Element106.remove()
+Element107.remove()
+Element108.remove()
+Element109.remove()
+Element110.remove()
+Element111.remove()
+Element112.remove()
+Element113.remove()
+Element114.remove()
+Element115.remove()
+Element116.remove()
+Element117.remove()
+Element118.remove()
+Element119.remove()
+Element120.remove()
+Element121.remove()
